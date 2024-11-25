a42719ecd46b5e4d527800eb08c1d0d1d78f865b
bestlife.nagairyo <bestlife.nagairyo@gmail.com>
2024-11-25 14:15:45 +0900

    kadai:Data_creation_function_and_validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Post;
+use App\Http\Requests\PostStoreRequest;
 
 class PostController extends Controller
 {
@@ -16,4 +17,19 @@ class PostController extends Controller
     $post = Post::find($id);
     return view('posts.show', compact('post'));
   }
+  public function create() {
+    return view('posts.create');
+  }
+
+  public function store(PostStoreRequest $request) {
+    // フォームの入力内容をもとに、テーブルにデータを追加する
+    $post = new Post();
+    $post->title = $request->input('title');
+    $post->content = $request->input('content');
+    $post->save();
+
+    // リダイレクトさせる
+    return redirect("/posts");
+  }
+
 }
